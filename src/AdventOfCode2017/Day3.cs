ef1a4bd3fd5235604762cf349236836f146d59c3
Chris Blyth <blythmeister@gmail.com>
2017-12-04 10:22:19 +0000

    2017 days 3 and 4

---
+++ src/AdventOfCode2017/Day3.cs
@@ -0,0 +1,128 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace AdventOfCode2017
+{
+    [TestFixture, Parallelizable(ParallelScope.All)]
+    public class Day3
+    {
+        private readonly int realData = 277678;
+
+        [TestCase(1, 0)]
+        [TestCase(12, 3)]
+        [TestCase(23, 2)]
+        [TestCase(1024, 31)]
+        [TestCase(null, 475)]
+        public void Part1(int? input, int? expected)
+        {
+            input = input ?? realData;
+
+            var n = 0;
+
+            for (int i = 0; i < input.Value; i++)
+            {
+                if (i * i > input.Value)
+                {
+                    n = i;
+                    break;
+                }
+            }
+
+            int[,] matrix = new int[n, n];
+            int row = 0;
+            int col = 0;
+            string direction = "right";
+
+            for (int i = n * n; i > 0; i--)
+            {
+                if (direction == "right" && (col > n - 1 || matrix[row, col] != 0))
+                {
+                    direction = "down";
+                    col--;
+                    row++;
+                }
+                if (direction == "down" && (row > n - 1 || matrix[row, col] != 0))
+                {
+                    direction = "left";
+                    row--;
+                    col--;
+                }
+                if (direction == "left" && (col < 0 || matrix[row, col] != 0))
+                {
+                    direction = "up";
+                    col++;
+                    row--;
+                }
+
+                if (direction == "up" && row < 0 || matrix[row, col] != 0)
+                {
+                    direction = "right";
+                    row++;
+                    col++;
+                }
+
+                matrix[row, col] = i;
+
+                if (direction == "right")
+                {
+                    col++;
+                }
+                if (direction == "down")
+                {
+                    row++;
+                }
+                if (direction == "left")
+                {
+                    col--;
+                }
+                if (direction == "up")
+                {
+                    row--;
+                }
+            }
+
+            if (direction == "right" && col > 0)
+            {
+                col--;
+            }
+            if (direction == "down" && row > 0)
+            {
+                row--;
+            }
+            if (direction == "left")
+            {
+                col++;
+            }
+            if (direction == "up")
+            {
+                row++;
+            }
+
+            var inputRow = 0;
+            var inputColumn = 0;
+
+            for (int rowIndex = 0; rowIndex < n; rowIndex++)
+            {
+                for (int columnIndex = 0; columnIndex < n; columnIndex++)
+                {
+                    var item = matrix[rowIndex, columnIndex];
+                    if (item == input.Value)
+                    {
+                        inputRow = rowIndex;
+                        inputColumn = columnIndex;
+                    }
+                }
+            }
+
+            var rowDistance = Math.Abs(row - inputRow);
+            var columnDistance = Math.Abs(col - inputColumn);
+            var result = rowDistance + columnDistance;
+
+            if (expected != null)
+            {
+                Assert.AreEqual(expected.Value, result);
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ src/AdventOfCode2017/Day4.cs
@@ -0,0 +1,79 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode2017
+{
+    [TestFixture, Parallelizable(ParallelScope.All)]
+    public class Day4
+    {
+        private readonly string realData = File.ReadAllText(Path.Combine(TestContext.CurrentContext.TestDirectory, "Data", "Day4.dat"));
+
+        [TestCase("aa bb cc dd ee", 1)]
+        [TestCase("aa bb cc dd aa", 0)]
+        [TestCase("aa bb cc dd aaa", 1)]
+        [TestCase(null, 325)]
+        public void Part1(string input, int? expected)
+        {
+            input = input ?? realData;
+
+            var result = 0;
+            foreach (var line in input.Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.None))
+            {
+                var parts = line.Split(new[] { " " }, StringSplitOptions.None);
+                var distinctParts = parts.Distinct();
+
+                if (parts.Length == distinctParts.Count())
+                {
+                    result++;
+                }
+            }
+
+            if (expected != null)
+            {
+                Assert.AreEqual(expected.Value, result);
+            }
+            Console.WriteLine(result);
+        }
+
+        [TestCase("abcde fghij", 1)]
+        [TestCase("abcde xyz ecdab", 0)]
+        [TestCase("a ab abc abd abf abj", 1)]
+        [TestCase("iiii oiii ooii oooi oooo", 1)]
+        [TestCase("oiii ioii iioi iiio", 0)]
+        [TestCase(null, 119)]
+        public void Part2(string input, int? expected)
+        {
+            input = input ?? realData;
+
+            var result = 0;
+            foreach (var line in input.Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.None))
+            {
+                var parts = line.Split(new[] { " " }, StringSplitOptions.None);
+                var partsOrdered = new List<string>();
+
+                foreach (var part in parts)
+                {
+                    var chars = part.ToLower().ToCharArray();
+                    Array.Sort(chars);
+                    partsOrdered.Add(new string(chars));
+                }
+
+                var distinctParts = partsOrdered.Distinct().ToList();
+
+                if (partsOrdered.Count() == distinctParts.Count())
+                {
+                    result++;
+                }
+            }
+
+            if (expected != null)
+            {
+                Assert.AreEqual(expected.Value, result);
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
