1a00e5d5d7b71d932793b2ab5349e58d9eb86909
Chris Blyth <blythmeister@gmail.com>
2017-12-04 08:43:33 +0000

    2016 Days 3, 4 & 5

--- src/AdventOfCode2016/Day3.cs
+++ src/AdventOfCode2016/Day3.cs
@@ -1,6 +1,7 @@
 ﻿using NUnit.Framework;
 using System;
 using System.IO;
+using System.Linq;
 
 namespace AdventOfCode2016
 {
@@ -9,11 +10,26 @@ namespace AdventOfCode2016
     {
         private readonly string realData = File.ReadAllText(Path.Combine(TestContext.CurrentContext.TestDirectory, "Data", "Day3.dat"));
 
-        //[TestCase(null, 1089)]
+        [TestCase("1,2,2", 1)]
+        [TestCase("1,1,3", 0)]
+        [TestCase("1,2,2\n1,1,3", 1)]
+        [TestCase("1,2,2\n4,1,1", 1)]
+        [TestCase(null, 1050)]
         public void Part1(string input, int? expected)
         {
             input = input ?? realData;
+
+            var tests = input.Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.None);
             var result = 0;
+
+            foreach (var test in tests)
+            {
+                var testParts = test.Split(',').Where(t => !string.IsNullOrWhiteSpace(t)).Select(t => int.Parse(t.Trim())).ToArray();
+                if (((testParts[1] + testParts[2]) > testParts[0]) && ((testParts[0] + testParts[2]) > testParts[1]) && ((testParts[0] + testParts[1]) > testParts[2]))
+                {
+                    result++;
+                }
+            }
 
             if (expected != null)
             {
@@ -22,11 +38,32 @@ namespace AdventOfCode2016
             Console.WriteLine(result);
         }
 
-        //[TestCase(null, 1156)]
+        [TestCase("1,1\n2,1\n2,3", 1)]
+        [TestCase("1,4\n2,1\n2,1", 1)]
+        [TestCase("102,302,502\n101,301,501\n103,303,503\n201,401,601\n202,402,602\n203,403,603", 6)]
+        [TestCase(null, 1921)]
         public void Part2(string input, int? expected)
         {
             input = input ?? realData;
             var result = 0;
+
+            var lines = input.Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.None);
+            var values = lines.Select(x => x.Split(',').Where(t => !string.IsNullOrWhiteSpace(t)).Select(t => int.Parse(t.Trim())).ToArray()).ToArray();
+
+            for (int i = 0; i < values.Length; i = i + 3)
+            {
+                var line1 = values[i];
+                var line2 = values[i + 1];
+                var line3 = values[i + 2];
+
+                for (int k = 0; k < line1.Length; k++)
+                {
+                    if (((line2[k] + line3[k]) > line1[k]) && ((line1[k] + line3[k]) > line2[k]) && ((line1[k] + line2[k]) > line3[k]))
+                    {
+                        result++;
+                    }
+                }
+            }
 
             if (expected != null)
             {
--- src/AdventOfCode2016/Day5.cs
+++ src/AdventOfCode2016/Day5.cs
@@ -1,32 +1,78 @@
 ﻿using NUnit.Framework;
 using System;
-using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace AdventOfCode2016
 {
     [TestFixture, Parallelizable(ParallelScope.All)]
     public class Day5
     {
-        private readonly string realData = File.ReadAllText(Path.Combine(TestContext.CurrentContext.TestDirectory, "Data", "Day5.dat"));
+        private readonly string realData = "ojvtpuvg";
 
-        //[TestCase(null, 1089)]
-        public void Part1(string input, int? expected)
+        [TestCase("abc", "18f47a30", Ignore = "Long Running")]
+        [TestCase(null, "4543c154", Ignore = "Long Running")]
+        public void Part1(string input, string expected)
         {
             input = input ?? realData;
-            var result = 0;
+            var result = "";
+
+            var md5 = MD5.Create();
+            for (int i = 0; i <= int.MaxValue; i++)
+            {
+                var computedHash = md5.ComputeHash(Encoding.ASCII.GetBytes(string.Format("{0}{1}", input, i)));
+                var hexString = BitConverter.ToString(computedHash).Replace("-", "").ToLower();
+                if (hexString.StartsWith("00000"))
+                {
+                    result += hexString[5];
+                    if (result.Length == 8)
+                    {
+                        break;
+                    }
+                }
+            }
 
             if (expected != null)
             {
-                Assert.AreEqual(expected.Value, result);
+                Assert.AreEqual(expected, result);
             }
             Console.WriteLine(result);
         }
 
-        //[TestCase(null, 1156)]
+        [TestCase("abc", "05ace8e3", Ignore = "Long Running")]
+        [TestCase(null, "1050cbbd", Ignore = "Long Running")]
         public void Part2(string input, int? expected)
         {
             input = input ?? realData;
-            var result = 0;
+            var password = new string[8];
+            var md5 = MD5.Create();
+            for (int i = 0; i <= int.MaxValue; i++)
+            {
+                var computedHash = md5.ComputeHash(Encoding.ASCII.GetBytes(string.Format("{0}{1}", input, i)));
+                var hexString = BitConverter.ToString(computedHash).Replace("-", "").ToLower();
+                if (hexString.StartsWith("00000"))
+                {
+                    var position = hexString[5];
+                    var character = hexString[6];
+                    int positionNumber;
+
+                    if (int.TryParse(position.ToString(), out positionNumber) && positionNumber < password.Length)
+                    {
+                        if (string.IsNullOrWhiteSpace(password[positionNumber]))
+                        {
+                            password[positionNumber] = character.ToString();
+                        }
+                    }
+
+                    if (password.All(p => !string.IsNullOrWhiteSpace(p)))
+                    {
+                        break;
+                    }
+                }
+            }
+
+            var result = string.Join("", password);
 
             if (expected != null)
             {
