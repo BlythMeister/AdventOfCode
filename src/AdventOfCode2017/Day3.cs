c7e2a5a7eb1fdcf508d9074e908b59481c4b4470
Chris Blyth <blythmeister@gmail.com>
2017-12-05 09:06:03 +0000

    Add Day3 part 2 solution and re-work part 1 The part 1 rework makes part 2 possible.

--- src/AdventOfCode2017/Day3.cs
+++ src/AdventOfCode2017/Day3.cs
@@ -1,5 +1,6 @@
 ﻿using NUnit.Framework;
 using System;
+using System.Collections.Generic;
 
 namespace AdventOfCode2017
 {
@@ -28,95 +29,165 @@ namespace AdventOfCode2017
                 }
             }
 
-            int[,] matrix = new int[n, n];
-            int row = 0;
-            int col = 0;
-            string direction = "right";
+            n = n + 3;
 
-            for (int i = n * n; i > 0; i--)
+            var matrix = new int[n, n];
+            var direction = "right";
+            var currentRow = (int)Math.Floor(n / 2d);
+            var currentCol = (int)Math.Floor(n / 2d);
+            var currentNumber = 1;
+            matrix[currentRow, currentCol] = currentNumber;
+            currentNumber++;
+
+            while (currentNumber <= input)
             {
-                if (direction == "right" && (col > n - 1 || matrix[row, col] != 0))
-                {
-                    direction = "down";
-                    col--;
-                    row++;
-                }
-                if (direction == "down" && (row > n - 1 || matrix[row, col] != 0))
-                {
-                    direction = "left";
-                    row--;
-                    col--;
-                }
-                if (direction == "left" && (col < 0 || matrix[row, col] != 0))
-                {
-                    direction = "up";
-                    col++;
-                    row--;
-                }
-
-                if (direction == "up" && row < 0 || matrix[row, col] != 0)
-                {
-                    direction = "right";
-                    row++;
-                    col++;
-                }
-
-                matrix[row, col] = i;
-
                 if (direction == "right")
                 {
-                    col++;
+                    currentCol++;
                 }
-                if (direction == "down")
+                else if (direction == "up")
                 {
-                    row++;
+                    currentRow--;
                 }
-                if (direction == "left")
+                else if (direction == "left")
                 {
-                    col--;
+                    currentCol--;
                 }
-                if (direction == "up")
+                else if (direction == "down")
                 {
-                    row--;
+                    currentRow++;
+                }
+
+                matrix[currentRow, currentCol] = currentNumber;
+                currentNumber++;
+
+                if (direction == "right" && matrix[currentRow - 1, currentCol] == 0)
+                {
+                    direction = "up";
+                }
+                else if (direction == "up" && matrix[currentRow, currentCol - 1] == 0)
+                {
+                    direction = "left";
+                }
+                else if (direction == "left" && matrix[currentRow + 1, currentCol] == 0)
+                {
+                    direction = "down";
+                }
+                else if (direction == "down" && matrix[currentRow, currentCol + 1] == 0)
+                {
+                    direction = "right";
                 }
             }
 
-            if (direction == "right" && col > 0)
-            {
-                col--;
-            }
-            if (direction == "down" && row > 0)
-            {
-                row--;
-            }
-            if (direction == "left")
-            {
-                col++;
-            }
-            if (direction == "up")
-            {
-                row++;
-            }
+            var startRow = (int)Math.Floor(n / 2d);
+            var startCol = (int)Math.Floor(n / 2d);
+            var rowDistance = Math.Abs(startRow - currentRow);
+            var columnDistance = Math.Abs(startCol - currentCol);
+            var result = rowDistance + columnDistance;
 
-            var inputRow = 0;
-            var inputColumn = 0;
-
-            for (int rowIndex = 0; rowIndex < n; rowIndex++)
+            if (expected != null)
             {
-                for (int columnIndex = 0; columnIndex < n; columnIndex++)
+                Assert.AreEqual(expected.Value, result);
+            }
+            Console.WriteLine(result);
+        }
+
+        [TestCase(1, 2)]
+        [TestCase(5, 10)]
+        [TestCase(10, 11)]
+        [TestCase(700, 747)]
+        [TestCase(null, 279138)]
+        public void Part2(int? input, int? expected)
+        {
+            input = input ?? realData;
+
+            var n = 0;
+
+            for (int i = 0; i < input.Value; i++)
+            {
+                if (i * i > input.Value)
                 {
-                    var item = matrix[rowIndex, columnIndex];
-                    if (item == input.Value)
+                    n = i;
+                    break;
+                }
+            }
+
+            n = n + 3;
+
+            var matrix = new int[n, n];
+            var direction = "right";
+            var currentRow = (int)Math.Floor(n / 2d);
+            var currentCol = (int)Math.Floor(n / 2d);
+            var currentNumber = 1;
+            matrix[currentRow, currentCol] = currentNumber;
+
+            while (currentNumber <= input)
+            {
+                if (direction == "right")
+                {
+                    currentCol++;
+                }
+                else if (direction == "up")
+                {
+                    currentRow--;
+                }
+                else if (direction == "left")
+                {
+                    currentCol--;
+                }
+                else if (direction == "down")
+                {
+                    currentRow++;
+                }
+
+                var positionsToAdd = new List<Tuple<int, int>>
+                {
+                    new Tuple<int, int>(currentRow - 1, currentCol -1),
+                    new Tuple<int, int>(currentRow - 1, currentCol),
+                    new Tuple<int, int>(currentRow - 1, currentCol + 1),
+                    new Tuple<int, int>(currentRow, currentCol -1),
+                    new Tuple<int, int>(currentRow, currentCol + 1),
+                    new Tuple<int, int>(currentRow + 1, currentCol -1),
+                    new Tuple<int, int>(currentRow + 1, currentCol),
+                    new Tuple<int, int>(currentRow + 1, currentCol + 1)
+                };
+
+                var newNumber = 0;
+                foreach (var position in positionsToAdd)
+                {
+                    try
                     {
-                        inputRow = rowIndex;
-                        inputColumn = columnIndex;
+                        var item = matrix[position.Item1, position.Item2];
+                        newNumber += item;
+                    }
+                    catch (Exception)
+                    {
+                        //Ignore
                     }
                 }
+
+                matrix[currentRow, currentCol] = newNumber;
+                currentNumber = newNumber;
+
+                if (direction == "right" && matrix[currentRow - 1, currentCol] == 0)
+                {
+                    direction = "up";
+                }
+                else if (direction == "up" && matrix[currentRow, currentCol - 1] == 0)
+                {
+                    direction = "left";
+                }
+                else if (direction == "left" && matrix[currentRow + 1, currentCol] == 0)
+                {
+                    direction = "down";
+                }
+                else if (direction == "down" && matrix[currentRow, currentCol + 1] == 0)
+                {
+                    direction = "right";
+                }
             }
 
-            var rowDistance = Math.Abs(row - inputRow);
-            var columnDistance = Math.Abs(col - inputColumn);
-            var result = rowDistance + columnDistance;
+            var result = currentNumber;
 
             if (expected != null)
             {
