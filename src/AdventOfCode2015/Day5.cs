32f515d07187a038fe6c6e65ac5b76856ff5f5e6
Chris Blyth <blythmeister@gmail.com>
2017-12-15 20:31:20 +0000

    2015 day 5 & 6

--- src/AdventOfCode2015/Day5.cs
+++ src/AdventOfCode2015/Day5.cs
@@ -49,6 +49,65 @@ namespace AdventOfCode2015
             }
             Console.WriteLine(result);
         }
+
+        [TestCase("qjhvhtzxzqqjkmpb", 1)]
+        [TestCase("xxyxx", 1)]
+        [TestCase("uurcxstgmygtbstg", 0)]
+        [TestCase("ieodomkazucvgmuy", 0)]
+        [TestCase("aaa", 0)]
+        [TestCase("abcdefeghi", 0)]
+        [TestCase("abcdefeghghi", 1)]
+        [TestCase(null, 53)]
+        public void Part2(string input, int? expected)
+        {
+            input = input ?? realData;
+            var result = 0;
+
+            foreach (var line in input.Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.None))
+            {
+                //Now, a nice string is one with all of the following properties:
+
+                //It contains a pair of any two letters that appears at least twice in the string without overlapping, like xyxy(xy) or aabcdefgaa(aa), but not like aaa(aa, but it overlaps).
+                //It contains at least one letter which repeats with exactly one letter between them, like xyx, abcdefeghi(efe), or even aaa.
+
+                var foundDouble = false;
+                for (int i = 0; i < line.Length - 1; i++)
+                {
+                    var preStr = line.Substring(0, i);
+                    var str = line.Substring(i, 2);
+                    var postStr = line.Substring(i + 2);
+
+                    if (preStr.Contains(str) || postStr.Contains(str))
+                    {
+                        foundDouble = true;
+                    }
+
+                }
+                if (!foundDouble) continue;
+
+                var foundLetterLoop = false;
+                for (int i = 0; i < line.Length - 2; i++)
+                {
+                    var current = line[i];
+                    var next = line[i + 2];
+
+                    if (current == next)
+                    {
+                        foundLetterLoop = true;
+                        break;
+                    }
+                }
+                if (!foundLetterLoop) continue;
+
+                result++;
+            }
+
+            if (expected != null)
+            {
+                Assert.AreEqual(expected.Value, result);
+            }
+            Console.WriteLine(result);
+        }
     }
 }
 
---
+++ src/AdventOfCode2015/Day6.cs
@@ -0,0 +1,158 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode2015
+{
+    [TestFixture, Parallelizable(ParallelScope.All)]
+    public class Day6
+    {
+        private readonly string realData = File.ReadAllText(Path.Combine(TestContext.CurrentContext.TestDirectory, "Data", "Day6.dat"));
+
+        [TestCase("turn on 0,0 through 999,999", 1000000)]
+        [TestCase("toggle 0,0 through 999,0", 1000)]
+        [TestCase("turn on 0,0 through 999,999\nturn off 499,499 through 500,500", 999996)]
+        [TestCase(null, 400410, Explicit = true)]
+        public void Part1(string input, int? expected)
+        {
+            input = input ?? realData;
+            var result = 0;
+
+            var grid = new Dictionary<(int, int), bool>();
+            for (int i = 0; i <= 999; i++)
+            {
+                for (int j = 0; j <= 999; j++)
+                {
+                    grid.Add((i, j), false);
+                }
+            }
+
+            foreach (var line in input.Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.None))
+            {
+                string action;
+                if (line.StartsWith("turn on"))
+                {
+                    action = "turn on";
+                }
+                else if (line.StartsWith("turn off"))
+                {
+                    action = "turn off";
+                }
+                else
+                {
+                    action = "toggle";
+                }
+
+                var remainingText = line.Substring(action.Length + 1);
+                var start = remainingText.Substring(0, remainingText.IndexOf("through")).Trim().Split(',').Select(int.Parse).ToList();
+                var end = remainingText.Substring(remainingText.IndexOf("through") + 8).Trim().Split(',').Select(int.Parse).ToList();
+
+                for (int i = start[0]; i <= end[0]; i++)
+                {
+                    for (int j = start[1]; j <= end[1]; j++)
+                    {
+                        switch (action)
+                        {
+                            case "turn on":
+                                grid[(i, j)] = true;
+                                break;
+                            case "turn off":
+                                grid[(i, j)] = false;
+                                break;
+                            case "toggle":
+                                grid[(i, j)] = !grid[(i, j)];
+                                break;
+                        }
+
+
+                    }
+                }
+            }
+
+            result = grid.Count(x => x.Value);
+
+            if (expected != null)
+            {
+                Assert.AreEqual(expected.Value, result);
+            }
+            Console.WriteLine(result);
+        }
+
+        [TestCase("turn on 0,0 through 999,999", 1000000)]
+        [TestCase("toggle 0,0 through 999,0", 2000)]
+        [TestCase("toggle 0,0 through 999,999", 2000000)]
+        [TestCase("turn on 0,0 through 999,999\nturn off 499,499 through 500,500", 999996)]
+        [TestCase(null, 15343601, Explicit = true)]
+        public void Part2(string input, int? expected)
+        {
+            input = input ?? realData;
+            var result = 0;
+
+            var grid = new Dictionary<(int, int), int>();
+            for (int i = 0; i <= 999; i++)
+            {
+                for (int j = 0; j <= 999; j++)
+                {
+                    grid.Add((i, j), 0);
+                }
+            }
+
+            foreach (var line in input.Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.None))
+            {
+                string action;
+                if (line.StartsWith("turn on"))
+                {
+                    action = "turn on";
+                }
+                else if (line.StartsWith("turn off"))
+                {
+                    action = "turn off";
+                }
+                else
+                {
+                    action = "toggle";
+                }
+
+                var remainingText = line.Substring(action.Length + 1);
+                var start = remainingText.Substring(0, remainingText.IndexOf("through")).Trim().Split(',').Select(int.Parse).ToList();
+                var end = remainingText.Substring(remainingText.IndexOf("through") + 8).Trim().Split(',').Select(int.Parse).ToList();
+
+                for (int i = start[0]; i <= end[0]; i++)
+                {
+                    for (int j = start[1]; j <= end[1]; j++)
+                    {
+                        switch (action)
+                        {
+                            case "turn on":
+                                grid[(i, j)] = grid[(i, j)] + 1;
+                                break;
+                            case "turn off":
+                                grid[(i, j)] = grid[(i, j)] - 1;
+                                break;
+                            case "toggle":
+                                grid[(i, j)] = grid[(i, j)] + 2;
+                                break;
+                        }
+
+                        if (grid[(i, j)] < 0)
+                        {
+                            grid[(i, j)] = 0;
+                        }
+
+                    }
+                }
+            }
+
+            result = grid.Sum(x => x.Value);
+
+            if (expected != null)
+            {
+                Assert.AreEqual(expected.Value, result);
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
+
