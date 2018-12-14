ec427746f4675a4c37751b4bef71a3e6ff395ce4
Chris Blyth <blythmeister@gmail.com>
2018-12-14 11:56:23 +0000

    Adding 2018

---
+++ src/AdventOfCode2018/Day1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using NUnit.Framework;
+
+namespace AdventOfCode2018
+{
+    [TestFixture, Parallelizable(ParallelScope.All)]
+    public class Day1
+    {
+        private readonly string[] realData = File.ReadAllLines(Path.Combine(TestContext.CurrentContext.TestDirectory, "Data", "Day1.dat"));
+
+        [TestCase("+1, +1, +1", 3)]
+        [TestCase("+1, +1, -2", 0)]
+        [TestCase("-1, -2, -3", -6)]
+        [TestCase(null, 477)]
+        public void Part1(string input, int? expected)
+        {
+            var lines = input != null ? input.Split(',') : realData;
+            var result = Execute(lines, false);
+
+            if (expected != null)
+            {
+                Assert.AreEqual(expected.Value, result);
+            }
+            Console.WriteLine(result);
+        }
+
+        [TestCase("+1, -1", 0)]
+        [TestCase("+3, +3, +4, -2, -4", 10)]
+        [TestCase("-6, +3, +8, +5, -6", 5)]
+        [TestCase("+7, +7, -2, -7, -4", 14)]
+        [TestCase(null, 390)]
+        public void Part2(string input, int? expected)
+        {
+            var lines = input != null ? input.Split(',') : realData;
+            var result = Execute(lines, true);
+
+            if (expected != null)
+            {
+                Assert.AreEqual(expected.Value, result);
+            }
+            Console.WriteLine(result);
+        }
+
+        private int Execute(string[] input, bool returnMatch)
+        {
+            var result = 0;
+            var seenNumbers = new List<int> {0};
+
+            while (true)
+            {
+                foreach (var s in input)
+                {
+                    var num = int.Parse(s);
+                    result = result + num;
+
+                    if (returnMatch && seenNumbers.Contains(result))
+                    {
+                        return result;
+                    }
+
+                    seenNumbers.Add(result);
+                }
+
+                if (!returnMatch) return result;
+            }
+        }
+    }
+}
---
+++ src/AdventOfCode2018/Day2.cs
@@ -0,0 +1,86 @@
+﻿using NUnit.Framework;
+using System;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode2018
+{
+    [TestFixture, Parallelizable(ParallelScope.All)]
+    public class Day2
+    {
+        private readonly string[] realData = File.ReadAllLines(Path.Combine(TestContext.CurrentContext.TestDirectory, "Data", "Day2.dat"));
+
+        [TestCase("abcdef,bababc,abbcde,abcccd,aabcdd,abcdee,ababab", 12)]
+        [TestCase(null, 4920)]
+        public void Part1(string input, int? expected)
+        {
+            var lines = input != null ? input.Split(',') : realData;
+
+            var twoLetter = 0;
+            var threeLetter = 0;
+
+            foreach (var s in lines)
+            {
+                var letters = s.GroupBy(x => x);
+                if (letters.Any(x => x.Count() == 2)) twoLetter++;
+                if (letters.Any(x => x.Count() == 3)) threeLetter++;
+            }
+
+            var result = twoLetter * threeLetter;
+
+            if (expected != null)
+            {
+                Assert.AreEqual(expected.Value, result);
+            }
+            Console.WriteLine(result);
+        }
+
+        [TestCase("abcde,fghij,klmno,pqrst,fguij,axcye,wvxyz", "fgij")]
+        [TestCase(null, "fonbwmjquwtapeyzikghtvdxl")]
+        public void Part2(string input, string expected)
+        {
+            var lines = input != null ? input.Split(',') : realData;
+            var result = string.Empty;
+            for (int i = 0; i < lines.Length; i++)
+            {
+                var line1 = lines[i];
+                for (int j = 0; j < lines.Length; j++)
+                {
+                    if (i == j) continue;
+                    var line2 = lines[j];
+                    var differentCharacters = 0;
+                    for (int k = 0; k < line1.Length; k++)
+                    {
+                        if (line1[k] != line2[k])
+                        {
+                            differentCharacters++;
+                        }
+                        else
+                        {
+                            result += line1[k];
+                        }
+                    }
+
+                    if (differentCharacters == 1)
+                    {
+                        break;
+                    }
+
+                    result = string.Empty;
+                }
+
+                if (!string.IsNullOrWhiteSpace(result))
+                {
+                    break;
+                }
+            }
+
+
+            if (expected != null)
+            {
+                Assert.AreEqual(expected, result);
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
